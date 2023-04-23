3c80292f3b34dd6038b041f9dfb033918e95f605
Fabio Kusaba <fabinhokb44@gmail.com>
2023-04-23 20:17:19 -0300

    Associar o Label com o Input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 
-import Label from './objects/Label';
 import Input from './objects/Input';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header';
@@ -11,8 +10,7 @@ const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
-    <Input value="show"/>
-    <Label content="Mostrar eventos"/>
+    <Input id="show" value="show" content="Mostrar eventos" />
   </main>
 );
 
