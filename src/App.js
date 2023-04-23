0bc08ae519dbb55adb3622bb3896a363d067e4f9
Fabio Kusaba <fabinhokb44@gmail.com>
2023-04-23 20:05:42 -0300

    Criamos o object Input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+
 import Label from './objects/Label';
+import Input from './objects/Input';
 import Hashtag from './components/Hashtag';
 import Header from './components/Header';
+
 
 const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
+    <Input value="show"/>
     <Label content="Mostrar eventos"/>
   </main>
 );
